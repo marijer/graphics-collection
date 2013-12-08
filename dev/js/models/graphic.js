cd57a544a3a80db10824b1354a7c1af96a105907
Marije Rooze <marijer@gmail.com>
2013-12-08 22:33:58 +0100

    url rewriting through value update

--- dev/js/models/graphic.js
+++ dev/js/models/graphic.js
@@ -2,7 +2,7 @@ APP.Graphic = Backbone.Model.extend ({
 	defaults: {
 		id: "",
 		title: "undefined",
-		date: null,
+		date: "unknown",
 		source: "unknown",
 		creators: "unknown",
 		favorite: false,
@@ -10,7 +10,19 @@ APP.Graphic = Backbone.Model.extend ({
 		news_type: "unknown",
 		annotation: "unknown",
 		flash: "unknown"
-	}
+	},
+
+	// rewrite of database values
+	parse: function(response){
+		paper = response.newspaper;
+		newscategory = response.newscategory;
+		
+		if (paper == "guardian") response.newspaper = "gua";
+		if (paper == "Ny Times") response.newspaper ="nyt"; 
+		if (newscategory == "Economy") response.newscategory = "business"; 
+
+		return response;
+ 	}
 });
 
 
